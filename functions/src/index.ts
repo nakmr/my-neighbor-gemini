f4e7b80423c09dc1847c2d5d716f1b016b12afd5
kimkim <nkimura2021infotech@gmail.com>
2024-07-14 16:05:19 +0900

    chore: Update firebase initialization in main.tsx

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,37 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { onRequest } from 'firebase-functions/v2/https'
+import * as logger from 'firebase-functions/logger'
+import * as admin from 'firebase-admin'
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const serviceAccountKeyPath = './serviceAccountKey.json'
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKeyPath),
+})
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const fireStore = admin.firestore()
+
+export const testStore = onRequest((request, response) => {
+  const params = request.body
+  const documentId = String(params.documentId)
+
+  if (documentId) {
+    const testRef = fireStore.collection('test')
+    testRef
+      .doc(documentId)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          response.status(200).send(doc.data())
+        } else {
+          response.status(200).send('document not found')
+        }
+      })
+  } else {
+    response.status(400).send({ errorMessaage: 'document id is not found' })
+  }
+})
+
+export const helloWorld = onRequest((request, response) => {
+  logger.info('Hello logs!', { structuredData: true })
+  response.send('Hello from Firebase!')
+})
